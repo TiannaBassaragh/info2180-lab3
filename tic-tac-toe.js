01a93c2f10cf7cf87021dd2254668bfce4d09641
TiannaBassaragh <tianna.bassaragh@outlook.com>
2025-10-17 23:06:28 -0500

    Exercise 2: added ability to place X or O and proper logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,4 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let square of squares) {
         square.classList.add("square");
     }
+
+    // Exercise 2
+    let currentPlayer = "X";                            // Track whose turn it is
+    const grid = ["", "", "", "", "", "", "", "", ""];  // Track state of the board
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener("click", () => {
+            // Only allow empty squares to be clicked
+            if (squares[i].textContent === "") {
+                squares[i].textContent = currentPlayer;
+                squares[i].classList.add(currentPlayer);
+                grid[i] = currentPlayer;
+
+                currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+                console.log(grid);
+            }
+        });
+    }
 });
